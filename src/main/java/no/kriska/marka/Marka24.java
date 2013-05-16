a0668d18f02df2647d550dfbb65bcf79e8464dcb
Kristoffer Skaret <kristofferskaret@Kristoffers-MacBook-Pro.local>
2013-05-16 11:48:47 +0200

    Faktorkalk 2013

--- src/main/java/no/kriska/marka/Marka24.java
+++ src/main/java/no/kriska/marka/Marka24.java
@@ -3,21 +3,23 @@ package no.kriska.marka;
 import java.util.Arrays;
 import java.util.List;
 
-import no.kriska.marka.graph.Faktorkalkulator2012;
+import no.kriska.marka.graph.Faktorkalkulator2013;
 import no.kriska.marka.graph.Graph;
 import no.kriska.marka.graph.GraphFactory;
 import no.kriska.marka.graph.Post;
 
 public class Marka24 {
 	public static void main(String[] args) {
-		Graph graph = new GraphFactory().fullGraph2(new Faktorkalkulator2012());
+		Graph graph = new GraphFactory().fullGraph2(new Faktorkalkulator2013());
 
 		RouteService routeService = new RouteService();
 
-		List<Post> skalBesoke = Arrays.asList();
+		// List<Post> skalBesoke = Arrays.asList();
 		// List<Post> skalBesoke = Arrays.asList(SKJENNUNGSTUA, STUDENTERHYTTA);
 		// List<Post> skalBesoke = Arrays.asList(Post.SAETEREN,
 		// Post.BRUNKOLLEN);
+		List<Post> skalBesoke = Arrays.asList(Post.SAETEREN, Post.BRUNKOLLEN,
+				Post.VENAS, Post.MYRSETER, Post.PRESTHYTTA);
 
 		routeService.optimalRoute(graph, 275, skalBesoke);
 	}
